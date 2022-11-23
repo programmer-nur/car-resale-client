4842ab35119022230d8ddbc70f535152c93eff48
nurmohammad83 <pesfootball83@gmail.com>
2022-11-24 02:51:38 +0600

    Add Categories

--- src/Pages/Home/CarItem.jsx
+++ src/Pages/Home/CarItem.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
 const CarItem = ({car}) => {
+    const {name,img,resaleprice,orginalprice,location,condition,time}=car
     return (
-        <div className="card bg-accent text-white shadow-xl">
-        <figure className="px-10 pt-10">
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-        </figure>
-        <div className="card-body ">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions w-full">
-            <button className="btn w-full btn-primary">Purchece</button>
+        <div className="card card-compact w-96 bg-base-700 shadow-xl">
+        <figure><img className='w-full' src={img} alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title text-left">{name}</h2>
+         <div className='text-left'>
+          <p><span className='text-lg font-medium'>Location:</span> {location}</p>
+          <p><span className='text-lg font-medium'>Resale Price</span> : {resaleprice}</p>
+          <p><span className='text-lg font-medium'>Original Price:</span> {orginalprice}</p>
+          <p><span className='text-lg font-medium'>Used:</span> {condition} Year</p>
+          <p><span className='text-lg font-medium'>Post:</span> {time}</p>
+         </div>
+          <div className="card-actions justify-end">
+            <button className="btn w-full btn-primary">Buy Now</button>
           </div>
         </div>
       </div>
