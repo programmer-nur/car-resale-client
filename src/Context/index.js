527edc8239e313d20e7473726c6d6da5294250fe
Nur Mohammad <mohammadnur5480@gmail.com>
2023-06-28 00:41:24 +0600

    Modify testimonial

--- src/Context/index.js
+++ src/Context/index.js
@@ -4,41 +4,47 @@ export const reviews=[
         name:'Sinthya',
         rating:4,
         img:'https://i.ibb.co/LrzRvgL/icons8-team-Fc-Lyt7l-W5wg-unsplash.jpg',
-        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, nostrum! this is Close'
+        des:'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec sed odio dui. Sed posuere consectetur est at lobortis. Maecenas faucibus mollis interdum. Aenean lacinia bibendum nulla sed.',
+        title:'C.E.O'
     },
     {
         _id:2,
         name:'Jhon',
         rating:5,
         img:'https://i.ibb.co/pxTwrm0/ian-dooley-d1-UPki-Fd04-A-unsplash.jpg',
-        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, nostrum! this is Close'
+        des:'Nullam id dolor cursus magna, vel scelerisque nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas faucibus mollis interdum. Praesent commodo cursus magna. Donec sed odio dui.',
+        title:'Manager'
     },
     {
         _id:3,
         name:'Saad',
         rating:5,
         img:'https://i.ibb.co/nzPQV5s/jd-chow-gutlcc-GLXKI-unsplash.jpg',
-        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, nostrum! this is Close'
+        des:'Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas faucibus mollis interdum. Praesent commodo cursus magna. Donec sed odio dui.',
+        title:'Sr. Web Developer'
     },
     {
         _id:4,
         name:'Mahin',
         rating:4,
         img:'https://i.ibb.co/LzLkXfY/marina-abrosimova-r8-Lm-Vb-UKgns-unsplash.jpg',
-        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, nostrum! this is Close'
+        des:'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Nullam quis risus eget urna.',
+        title:'Project Manager'
     },
     {
         _id:5,
         name:'Kim',
         rating:5,
         img:'https://i.ibb.co/zxBkw8k/julian-wan-WNo-Ln-Jo7t-S8-unsplash.jpg',
-        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, nostrum! this is Close'
+        des:'Maecenas faucibus mollis interdum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor gravida at eget metus. Vestibulum id ligula porta this is Close',
+        title:'Student'
     },
     {
         _id:6,
         name:'Trina',
         rating:4,
         img:'https://i.ibb.co/VMttqjW/joshua-rondeau-Zn-HRNtw-Xg6-Q-unsplash.jpg',
-        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, nostrum! this is Close'
+        des:'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas faucibus mollis interdum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor gravida at eget metus. Vestibulum id ligula porta.',
+        title:'Technical Requiter'
     },
 ]
--- src/Pages/Home/Review.jsx
+++ src/Pages/Home/Review.jsx
@@ -19,8 +19,6 @@ import {
 } from "swiper";
 import { reviews } from "../../Context";
 import {motion} from "framer-motion"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 const Review = () => {
   return (
     <section className="pt-16 min-h-screen max-w-6xl mx-auto">
@@ -30,18 +28,10 @@ const Review = () => {
       transition={{duration:.8}}
       className="text-5xl font-semibold text-center">Customer Feedback</motion.h2>
        <Swiper
-          effect={"coverflow"}
           grabCursor={true}
           centeredSlides={true}
           slidesPerView={"auto"}
           loop={true}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
           pagination={{
             clickable: true,
           }}
@@ -52,30 +42,20 @@ const Review = () => {
             delay: 2000,
             disableOnInteraction: false,
           }}
-          modules={[EffectCoverflow, Autoplay, Pagination, Keyboard, Navigation]}
+          modules={[EffectCoverflow ,Autoplay, Pagination, Keyboard, Navigation]}
           className="mySwiper"
         >
         {reviews.map((review) => (
-          <SwiperSlide key={review._id}>
-              
-              <div className="card w-[320px] h-[320px] lg:w-[400px] lg:h-[400px] bg-accent rounded-xl text-white shadow hover:shadow-xl hover:shadow-accent duration-75">
-      <div className="card-body rounded-md p-8 text-center">
-        <div className="avatar justify-center">
-          <div className="w-16 lg:w-24 mask rounded-full">
-            <img src={review.img} alt="" />
-          </div>
+          <SwiperSlide key={review._id}>         
+<div class=" mb-6 p-4">
+        <div class="h-full text-center">
+          <img alt="testimonial" class="w-16 h-16 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={review.img}/>
+          <p class="leading-relaxed">{review.des}</p>
+          <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+          <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">{review.name}</h2>
+          <p class="text-gray-500">{review.title}</p>
         </div>
-        <h2 className="text-2xl">{review.name}</h2>
-        <hr className="border-separate my-2 border-primary" />
-        <p>{review.des}</p>
-        <hr className="border-separate my-2 border-primary" />
-        <p className="text-xl">
-          Ratings &nbsp; {review.rating}{" "}
-          <FontAwesomeIcon icon={faStar} size="sm"></FontAwesomeIcon>
-        </p>
       </div>
-    </div>
-
         </SwiperSlide>
         ))}
       </Swiper>
