fb14048f3bcac1edb43d059654fe336961dd4b3f
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-25 13:01:42 +0600

    feat: added tailwind congih

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,14 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#ff7607",
-          secondary: "#f59e0b",
-          success: "#16a34a",
-          warning: "#dc2626",
-          accent: "#232F3C",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
-          "base-200": "#fff7ed",
-        },
+  mode: "jit",
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
       },
-      "dark",
-      "cupcake",
-    ],
+    },
   },
   plugins: [require("daisyui")],
 }
